39989c9d7f57d8cba5dd0518dd5e6be25ee566e9
supawadeeboonsa <baipo2511@gmail.com>
2025-09-20 01:06:05 +0700

    new5

--- src/app/teacher/[id]/page.tsx
+++ src/app/teacher/[id]/page.tsx
@@ -1,7 +1,12 @@
 // src/app/teacher/[id]/page.tsx
+"use client"; // ถ้าต้องการใช้ client components เช่น Zustand store
 import StudentDetail from "../../components/StudentDetail";
+import { useParams } from "next/navigation";
 
-export default function StudentDetailPage({ params }: { params: { id: string } }) {
+export default function StudentDetailPage() {
+  const params = useParams(); // ดึง id จาก route
+  if (!params?.id) return <div>ไม่พบ ID นักเรียน</div>;
+
   return (
     <div className="p-8">
       <StudentDetail studentId={params.id} />
